58f592d938ac738d5bc66b6ed1b505a43166cc7d
VladScv <ddcllrnsbrv@uoc.edu>
2022-09-28 14:30:03 +0200

    Console resize improved

--- js/starter.js
+++ js/starter.js
@@ -4,7 +4,28 @@ window.onresize=function (){
     clearTimeout(resizeTimer);
     resizeTimer=setTimeout(function (){resizeBody()},100);
 }
+window.onload=function (){
+    resizeBody();
+}
 function resizeBody() {
-    const console= document.getElementById('dp-console');
-    if(console.getBoundingClientRect().width>=console.style.minWidth*2) {console.style.setProperty("width","30rem")}
+    let consolePanel= document.getElementById('dp-console'); 
+    let num = calcREM(document.body.getBoundingClientRect().width/2);
+    console.log(num)
+    if(num  > 30) {
+        consolePanel.style.width=convertRemToPixels(30)+"px";
+    }else {
+        consolePanel.style.width=document.getElementById("dp-body").getBoundingClientRect().width+"px";
+    }
+}
+
+function calcREM(px)
+{
+    if( !isNaN(px) )
+    {
+        var remInPx = parseFloat($("html").css("font-size"));
+        return (parseFloat(px) / remInPx);
+    }
+}
+function convertRemToPixels(rem) {
+    return rem * parseFloat(getComputedStyle(document.documentElement).fontSize);
 }
