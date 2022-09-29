37a75ec6f8ad609a34e3168bbbeef59274cbc085
VladScv <ddcllrnsbrv@uoc.edu>
2022-09-29 17:40:03 +0200

    Closing brackets

---
+++ js/listeners.js
@@ -0,0 +1,25 @@
+export const closing1 = document.getElementById("dp-closing-1");
+export const closing2 = document.getElementById("dp-closing-2");
+export const closing3 = document.getElementById("dp-closing-3");
+
+$('#collapseOne').on('show.bs.collapse', function () {
+    closing1.style.visibility='hidden';
+})
+$('#collapseOne').on('hidden.bs.collapse', function () {
+    closing1.style.visibility='visible';
+})
+
+$('#collapseTwo').on('show.bs.collapse', function () {
+    closing2.style.visibility='hidden';
+})
+$('#collapseTwo').on('hidden.bs.collapse', function () {
+    closing2.style.visibility='visible';
+})
+
+$('#collapseThree').on('show.bs.collapse', function () {
+    closing3.style.visibility='hidden';
+})
+$('#collapseThree').on('hidden.bs.collapse', function () {
+    closing3.style.visibility='visible';
+})
+
