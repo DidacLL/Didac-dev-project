d627a19a87a6b34c3f03a18666e3c66cd8ea48ea
VladScv <ddcllrnsbrv@uoc.edu>
2022-09-29 02:13:22 +0200

    Closing brackets

--- js/starter.js
+++ js/starter.js
@@ -29,3 +29,41 @@ function calcREM(px)
 function convertRemToPixels(rem) {
     return rem * parseFloat(getComputedStyle(document.documentElement).fontSize);
 }
+
+const consoleEducation= document.getElementById("headingOne");
+const consoleSkills= document.getElementById("headingTwo");
+const consoleWorkExp= document.getElementById("headingThree");
+const consoleEducationBody= document.getElementById("collapseOne");
+const consoleSkillsBody= document.getElementById("collapseTwo");
+const consoleWorkExpBody= document.getElementById("collapseThree");
+const closing1= document.getElementById("dp-closing-1");
+const closing2= document.getElementById("dp-closing-2");
+const closing3= document.getElementById("dp-closing-3");
+
+
+$('#collapseOne').on('show.bs.collapse', function () {
+    closing1.style.visibility='hidden';
+})
+$('#collapseOne').on('hidden.bs.collapse', function () {
+    closing1.style.visibility='visible';
+})
+
+$('#collapseTwo').on('show.bs.collapse', function () {
+    closing2.style.visibility='hidden';
+})
+$('#collapseTwo').on('hidden.bs.collapse', function () {
+    closing2.style.visibility='visible';
+})
+
+$('#collapseThree').on('show.bs.collapse', function () {
+    closing3.style.visibility='hidden';
+})
+$('#collapseThree').on('hidden.bs.collapse', function () {
+    closing3.style.visibility='visible';
+})
+
+
+
+
+
+
